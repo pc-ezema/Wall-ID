9c21800b5eb631316842f0dc01193b29c6339c82
Promise <promiseezema11@gmail.com>
2022-06-22 13:53:09 +0100

    updated

--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://wall.victornwadinobi.com/api/';
+axios.defaults.baseURL = 'https://wall.victornwadinobi.com/api/';
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
