67ee509afd9fbc2a24291f95792b9a4f7728eb11
Gbadamosi Moshood <morshud@users.noreply.github.com>
2022-07-30 18:52:54 +0100

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -3,4 +3,4 @@ import axios from 'axios';
 // axios.defaults.baseURL = 'http://127.0.0.1:8000/api/';
 axios.defaults.baseURL = 'https://wall.victornwadinobi.com/api/';
 //axios.defaults.baseURL = 'https://wallid-backend.herokuapp.com/api/';
-axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
+axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
