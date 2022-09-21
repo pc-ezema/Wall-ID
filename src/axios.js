fc8366a432b504692f80c5fe717250ae13cb2a7d
Promise <promiseezema11@gmail.com>
2022-09-21 11:55:58 +0100

    updated

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 // axios.defaults.baseURL = 'http://127.0.0.1:8000/api/';
-axios.defaults.baseURL = "https://wall.victornwadinobi.com/api/";
+axios.defaults.baseURL = "https://wallid.herokuapp.com/api/";
 axios.defaults.headers.common["Authorization"] =
   "Bearer " + localStorage.getItem("token");
